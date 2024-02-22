a3d33a187c3635961ecbc1937e61e7f82d5ae9b2
xzhangfordham <42500181+xzhangfordham@users.noreply.github.com>
2024-02-21 21:01:15 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "BigInt.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#define SIZE 10
+
+int main()
+{
+  ds::BigInt a("1234567890123");
+  ds::BigInt b("1567890123");
+
+  a.output(); //this should display a's value: 1234567890123
+
+  ds:BigInt r;
+  BigInt::add (a, b, r);
+
+  r.output ();  //This should display 1236135780246
+
+  ds:BigInt r2;
+  BigInt::substract (a,b,r2);
+  
+  r2.output (); //This should display 123000000000
+
+  
+ 
+}
