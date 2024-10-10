8bd840c767c5c31a146e5ae5f826e112042cfe57
kjannath <137518400+kjannath@users.noreply.github.com>
2024-10-10 14:12:15 -0400

    updates

--- BigInt.h
+++ BigInt.h
@@ -11,17 +11,20 @@ namespace ds
   {
   public:
     BigInt() = default;
-    explicit BigInt(std::string str);
+    explicit BigInt(std::string str);  
     // Since we are not managing dynamic memory, we don't need the rule of three!
+    
     void output() { std::cout << *this << std::endl;}
     // BONUS: TODO: Implement the * operator
+    
     BigInt operator+(const BigInt& other) const 
     {
       BigInt result;
       BigInt::add(*this, other, result);
       return result;
     }
-    BigInt operator-(const BigInt& other) const 
+   
+   BigInt operator-(const BigInt& other) const 
     {
       BigInt result;
       if (*this >= other)
@@ -35,11 +38,14 @@ namespace ds
       return result;
     }
     // While this function strictly isn't required, it is helpful for `-` operator
+    
     bool operator>=(const BigInt& other) const;
     // This operator is used in testing / implementing output
     friend std::ostream& operator<<(std::ostream& os, const BigInt& d)
     {
-      // TODO: Implement this
+      
+      
+      return os;
     }
   private:
     // Add and Subtract are private as they are internal implementation details
@@ -54,11 +60,26 @@ ds::BigInt::BigInt(std::string str)
 {
   // TODO: Implement this
   // HINT: Can you use std::transform?
+  digits.reserve(str.size());
+  std::transform(str.rbegin(), str.rend(), std::back_inserter(digits), [](char ch) {
+  return ch-'0';
+  });
 }
 
 void ds::BigInt::add(const BigInt& a, const BigInt& b, BigInt& result) const
 {
   // TODO: Implement this
+    int carry = 0;
+    size_t maxSize = std::max(a.digits.size(), b.digits.size());
+
+    for (size_t i = 0; i < maxSize || carry; ++i){
+      int d1 = (i < a.digits.size()) ? a.digits[i] : 0;
+      int d2 = (i < b.digits.size()) ? b.digits[i] : 0;
+    
+      int sum = d1 + d2 + carry;
+      result.digits.push_back(sum % 10);
+      carry = sum / 10;
+    }
 }
 
 /**
@@ -72,10 +93,43 @@ void ds::BigInt::add(const BigInt& a, const BigInt& b, BigInt& result) const
 void ds::BigInt::subtract(const BigInt& a, const BigInt& b, BigInt& result) const
 {
   // TODO: Implement this 
+  int borrow = 0;
+
+  for (size_t i = 0; i < a.digits.size(); ++i){
+    int d1 = a.digits[i];
+    int d2 = (i < b.digits.size()) ? b.digits[i] : 0;
+
+    int diff = d1 - d2 - borrow;
+    if (diff < 0){
+      diff += 10;
+      borrow = 1;
+    }
+    else{
+      borrow = 0;
+    }
+    result.digits.push_back(diff);
+  }
+
+  // Remove leading zeros in the result
+  while (result.digits.size() > 1 && result.digits.back() == 0){
+    result.digits.pop_back();
+  }
 }
 
 // Needed to implement absolute difference
 bool ds::BigInt::operator>=(const BigInt& other) const
 {
-  // TODO: Implement this
+  // TODO: Implement 
+  if (digits.size() != other.digits.size()){
+    return digits.size() > other.digits.size();
+  }
+
+  // Compare from most significant to least significant
+  for (int i = digits.size() - 1; i >= 0; --i){
+    if (digits[i] != other.digits[i]){
+      return digits[i] > other.digits[i];
+    }
+  }
+
+  return true;  // They are equal
 }
